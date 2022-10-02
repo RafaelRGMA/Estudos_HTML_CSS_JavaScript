537cbb5a7664666309ba8aa35259be6adb9a8812
Rafael <rafael.rgma@gmail.com>
2022-10-02 08:17:11 -0300

    Comandos do teclado implementado

--- Calculadora/dist/modulosdeclasse/CalculadoraInterface.js
+++ Calculadora/dist/modulosdeclasse/CalculadoraInterface.js
@@ -11,88 +11,154 @@ export class CalculadoraInterface {
         this._btnMultiplicacao = document.querySelector("#multiplicacao");
         this._btnDivisao = document.querySelector("#divizao");
         this._btnIgual = document.querySelector("#igual");
+        this._btnLimpar = document.querySelector("#limpar");
         this._ativarBotoesNumericos();
         this._ativarBotaoSoma();
         this._ativarBotaoSubtracao();
         this._ativarBotaoMultiplicacao();
         this._ativarBotaoDivisao();
         this._ativarBotaoIgual();
+        this._ativarBotaoLimpar();
+        this._display.value = "0";
     }
+    //=====CONFIGURAÇÕES DOS BOTÕES NUMÉRICOS================================================
     _botoesNumericos() {
         return document.querySelectorAll(".numeric-button");
+    }
+    _inputNumero(botao) {
+        if (this._ultimaOperacao == "=") {
+            this._operacoes.limparResultado();
+            this._ultimaOperacao = "";
+        }
+        if (botao.innerText != ",") {
+            this._operacoes.memoria = this._operacoes.memoria + botao.innerText;
+        }
+        else {
+            this._operacoes.memoria = (this._operacoes.memoria.search(/[,]/) == -1) ? this._operacoes.memoria + botao.innerText : this._operacoes.memoria;
+        }
+        this._display.value = this._operacoes.memoria;
+        this._inputNumerico = true;
     }
     _ativarBotoesNumericos() {
         this._arrayBtnNum.forEach((botao) => {
             botao.addEventListener("click", () => {
-                if (this._ultimaOperacao == "=") {
-                    this._operacoes.limparResultado();
-                    this._ultimaOperacao = "";
+                this._inputNumero(botao);
+            });
+            document.addEventListener("keypress", (event) => {
+                if (event.key == botao.innerHTML) {
+                    this._inputNumero(botao);
                 }
-                if (botao.innerText != ",") {
-                    this._operacoes.memoria = this._operacoes.memoria + botao.innerText;
-                }
-                else {
-                    this._operacoes.memoria = (this._operacoes.memoria.search(/[,]/) == -1) ? this._operacoes.memoria + botao.innerText : this._operacoes.memoria;
-                }
-                this._display.value = this._operacoes.memoria;
-                this._inputNumerico = true;
             });
         });
     }
+    //=========================================================================================================
+    //======CONFIGURAÇÃO DO BOTÃO SOMAR========================================================================
+    _somar() {
+        if (this._inputNumerico && (this._ultimaOperacao != "x" && this._ultimaOperacao != "=")) {
+            this._operacoes.somaValor(this._retornaValorDisplay());
+        }
+        else if (this._inputNumerico && this._ultimaOperacao == "x") {
+            this._operacoes.multiplicaValor(this._retornaValorDisplay());
+        }
+        this._display.value = this._operacoes.resultado.toString().replace(".", ",");
+        this._inputNumerico = false;
+        this._ultimaOperacao = "+";
+    }
     _ativarBotaoSoma() {
         this._btnSoma.addEventListener("click", () => {
-            if (this._inputNumerico && (this._ultimaOperacao != "x" && this._ultimaOperacao != "=")) {
-                this._operacoes.somaValor(this._retornaValorDisplay());
-            }
-            else if (this._inputNumerico && this._ultimaOperacao == "x") {
-                this._operacoes.multiplicaValor(this._retornaValorDisplay());
-            }
-            this._display.value = this._operacoes.resultado.toString().replace(".", ",");
-            this._inputNumerico = false;
-            this._ultimaOperacao = "+";
+            this._somar();
         });
+        document.addEventListener("keypress", (event) => {
+            if (event.key == "+") {
+                this._somar();
+            }
+        });
+    }
+    //=================================================================================================================
+    //======CONFIGURAÇÃO DO BOTÃO SUBTRAIR=============================================================================
+    _subtrair() {
+        if (this._inputNumerico && (this._ultimaOperacao != "x" && this._ultimaOperacao != "=")) {
+            this._operacoes.somaValor(this._retornaValorDisplay());
+        }
+        else if (this._inputNumerico && this._ultimaOperacao == "x") {
+            this._operacoes.multiplicaValor(this._retornaValorDisplay());
+        }
+        this._operacoes.memoria = this._btnSubtracao.innerHTML;
+        this._display.value = this._operacoes.resultado.toString().replace(".", ",");
+        this._inputNumerico = false;
+        this._ultimaOperacao = "-";
     }
     _ativarBotaoSubtracao() {
         this._btnSubtracao.addEventListener("click", () => {
-            if (this._inputNumerico && (this._ultimaOperacao != "x" && this._ultimaOperacao != "=")) {
-                this._operacoes.somaValor(this._retornaValorDisplay());
-            }
-            else if (this._inputNumerico && this._ultimaOperacao == "x") {
-                this._operacoes.multiplicaValor(this._retornaValorDisplay());
-            }
-            this._operacoes.memoria = this._btnSubtracao.innerHTML;
-            this._display.value = this._operacoes.resultado.toString().replace(".", ",");
-            this._inputNumerico = false;
-            this._ultimaOperacao = "-";
+            this._subtrair();
         });
+        document.addEventListener("keypress", (event) => {
+            if (event.key == "-") {
+                this._subtrair();
+            }
+        });
+    }
+    //=================================================================================================================
+    //=========CONFIGURAÇÃO DO BOTÃO MULTIPLICAR=======================================================================
+    _multiplicar() {
+        if (this._ultimaOperacao != "x" && this._ultimaOperacao != "=") {
+            this._operacoes.somaValor(this._retornaValorDisplay());
+        }
+        else if (this._inputNumerico && this._ultimaOperacao == "x") {
+            this._operacoes.multiplicaValor(this._retornaValorDisplay());
+        }
+        this._display.value = this._operacoes.resultado.toString().replace(".", ",");
+        this._inputNumerico = false;
+        this._ultimaOperacao = "x";
     }
     _ativarBotaoMultiplicacao() {
         this._btnMultiplicacao.addEventListener("click", () => {
-            if (this._ultimaOperacao != "x" && this._ultimaOperacao != "=") {
-                this._operacoes.somaValor(this._retornaValorDisplay());
+            this._multiplicar();
+        });
+        document.addEventListener("keypress", (event) => {
+            if (event.key == "*") {
+                this._multiplicar();
             }
-            else if (this._inputNumerico && this._ultimaOperacao == "x") {
-                this._operacoes.multiplicaValor(this._retornaValorDisplay());
-            }
-            this._display.value = this._operacoes.resultado.toString().replace(".", ",");
-            this._inputNumerico = false;
-            this._ultimaOperacao = "x";
+            ;
         });
     }
+    //==================================================================================================================
+    //===============CONFIGURAÇÃO DO BOTÃO DIVIDIR======================================================================
     _ativarBotaoDivisao() {
+    }
+    //===============CONFIGURAÇÃO DO BOTÃO IGUAL========================================================================
+    _totalizar() {
+        if (this._inputNumerico && (this._ultimaOperacao == "+" || this._ultimaOperacao == "-")) {
+            this._operacoes.somaValor(this._retornaValorDisplay());
+        }
+        else if (this._inputNumerico && this._ultimaOperacao == "x") {
+            this._operacoes.multiplicaValor(this._retornaValorDisplay());
+        }
+        this._display.value = this._operacoes.resultado.toString().replace(".", ",");
+        this._ultimaOperacao = "=";
     }
     _ativarBotaoIgual() {
         this._btnIgual.addEventListener('click', () => {
-            if (this._inputNumerico && (this._ultimaOperacao == "+" || this._ultimaOperacao == "-")) {
-                this._operacoes.somaValor(this._retornaValorDisplay());
+            this._totalizar();
+        });
+        document.addEventListener("keypress", (event) => {
+            if (event.key == "Enter") {
+                this._totalizar();
             }
-            else if (this._inputNumerico && this._ultimaOperacao == "x") {
-                this._operacoes.multiplicaValor(this._retornaValorDisplay());
-            }
-            this._display.value = this._operacoes.resultado.toString().replace(".", ",");
-            this._ultimaOperacao = "=";
         });
     }
+    //===================================================================================================================
+    //========CONFIGURAÇÃO DO BOTÃO LIMPAR===============================================================================
+    _ativarBotaoLimpar() {
+        this._btnLimpar.addEventListener("click", () => {
+            this._display.value = "0";
+            this._operacoes.memoria = "";
+            this._operacoes.limparResultado();
+            this._inputNumerico = false;
+            this._ultimaOperacao = "";
+        });
+    }
+    //===================================================================================================================  
     _retornaValorDisplay() {
         return (this._display.value.search(/[,]/) == -1) ? parseInt(this._display.value) : parseFloat(this._display.value.replace(",", "."));
     }
--- Calculadora/src/modulosdeclasse/CalculadoraInterface.ts
+++ Calculadora/src/modulosdeclasse/CalculadoraInterface.ts
@@ -8,6 +8,7 @@ export class CalculadoraInterface{
     private _btnIgual:HTMLButtonElement;
     private _btnMultiplicacao:HTMLButtonElement;
     private _btnDivisao:HTMLButtonElement;
+    private _btnLimpar:HTMLButtonElement;
     private _inputNumerico:boolean = false;
     private _ultimaOperacao:string = ""
     private _operacoes:Operacoes;
@@ -20,101 +21,180 @@ export class CalculadoraInterface{
         this._btnMultiplicacao = document.querySelector("#multiplicacao");
         this._btnDivisao = document.querySelector("#divizao");
         this._btnIgual = document.querySelector("#igual");
+        this._btnLimpar = document.querySelector("#limpar");
         this._ativarBotoesNumericos();
         this._ativarBotaoSoma();
         this._ativarBotaoSubtracao();
         this._ativarBotaoMultiplicacao();
         this._ativarBotaoDivisao();
         this._ativarBotaoIgual();
+        this._ativarBotaoLimpar();
+        this._display.value = "0";
     }
 
   
-
+//=====CONFIGURAÇÕES DOS BOTÕES NUMÉRICOS================================================
     private _botoesNumericos():NodeList{
         return document.querySelectorAll(".numeric-button");
     }
 
+    private _inputNumero(botao:HTMLButtonElement):void{
+        if(this._ultimaOperacao == "="){
+            this._operacoes.limparResultado();
+            this._ultimaOperacao = "";
+        }
+
+        if(botao.innerText != ","){
+            this._operacoes.memoria = this._operacoes.memoria + botao.innerText;
+        }else{
+            this._operacoes.memoria = (this._operacoes.memoria.search(/[,]/) == -1)?this._operacoes.memoria + botao.innerText : this._operacoes.memoria;
+        }
+        
+        this._display.value = this._operacoes.memoria;
+        this._inputNumerico = true;
+    }
+
+
     private _ativarBotoesNumericos():void{
         this._arrayBtnNum.forEach( (botao:HTMLButtonElement)=>{
            botao.addEventListener("click",()=>{
-                if(this._ultimaOperacao == "="){
-                    this._operacoes.limparResultado();
-                    this._ultimaOperacao = "";
+               this._inputNumero(botao);                
+           });
+           document.addEventListener("keypress", (event)=>{
+                if(event.key == botao.innerHTML){
+                    this._inputNumero(botao);
                 }
-
-                if(botao.innerText != ","){
-                    this._operacoes.memoria = this._operacoes.memoria + botao.innerText;
-                }else{
-                    this._operacoes.memoria = (this._operacoes.memoria.search(/[,]/) == -1)?this._operacoes.memoria + botao.innerText : this._operacoes.memoria;
-                }
-                
-                this._display.value = this._operacoes.memoria;
-                this._inputNumerico = true;
-                
            });
         });
     }
 
+//=========================================================================================================
+
+//======CONFIGURAÇÃO DO BOTÃO SOMAR========================================================================
+    private _somar():void{
+        if(this._inputNumerico && (this._ultimaOperacao != "x" && this._ultimaOperacao != "=")){
+            this._operacoes.somaValor(this._retornaValorDisplay());
+        }else if(this._inputNumerico && this._ultimaOperacao == "x"){
+            this._operacoes.multiplicaValor(this._retornaValorDisplay());
+        }
+
+        this._display.value = this._operacoes.resultado.toString().replace(".", ",");          
+        this._inputNumerico = false;
+        this._ultimaOperacao = "+";
+    }
+
     private _ativarBotaoSoma():void{
         this._btnSoma.addEventListener("click", ()=>{
-            if(this._inputNumerico && (this._ultimaOperacao != "x" && this._ultimaOperacao != "=")){
-                this._operacoes.somaValor(this._retornaValorDisplay());
-            }else if(this._inputNumerico && this._ultimaOperacao == "x"){
-                this._operacoes.multiplicaValor(this._retornaValorDisplay());
-            }
-
-            this._display.value = this._operacoes.resultado.toString().replace(".", ",");          
-            this._inputNumerico = false;
-            this._ultimaOperacao = "+";
+           this._somar();
         });
+
+        document.addEventListener("keypress", (event)=>{
+            if(event.key == "+"){
+                this._somar();
+            }
+        });
+    }
+//=================================================================================================================
+
+
+//======CONFIGURAÇÃO DO BOTÃO SUBTRAIR=============================================================================
+    private _subtrair():void{
+        if(this._inputNumerico && (this._ultimaOperacao != "x" && this._ultimaOperacao != "=")){
+            this._operacoes.somaValor(this._retornaValorDisplay());
+        }else if(this._inputNumerico && this._ultimaOperacao == "x"){
+            this._operacoes.multiplicaValor(this._retornaValorDisplay());
+        }    
+
+        this._operacoes.memoria = this._btnSubtracao.innerHTML;
+        this._display.value = this._operacoes.resultado.toString().replace(".", ",");          
+        this._inputNumerico = false;
+        this._ultimaOperacao = "-"
     }
 
     private _ativarBotaoSubtracao():void{
         this._btnSubtracao.addEventListener("click", ()=>{
-            if(this._inputNumerico && (this._ultimaOperacao != "x" && this._ultimaOperacao != "=")){
-                this._operacoes.somaValor(this._retornaValorDisplay());
-            }else if(this._inputNumerico && this._ultimaOperacao == "x"){
-                this._operacoes.multiplicaValor(this._retornaValorDisplay());
-            }    
+            this._subtrair();
+        });
 
-            this._operacoes.memoria = this._btnSubtracao.innerHTML;
-            this._display.value = this._operacoes.resultado.toString().replace(".", ",");          
-            this._inputNumerico = false;
-            this._ultimaOperacao = "-"
+        document.addEventListener("keypress", (event)=>{
+            if(event.key == "-"){
+                this._subtrair();
+            }
         });
     }
+
+//=================================================================================================================
+
+//=========CONFIGURAÇÃO DO BOTÃO MULTIPLICAR=======================================================================
+    private _multiplicar():void{
+        if(this._ultimaOperacao != "x" && this._ultimaOperacao != "="){
+            this._operacoes.somaValor(this._retornaValorDisplay());
+        }else if(this._inputNumerico && this._ultimaOperacao == "x"){
+            this._operacoes.multiplicaValor(this._retornaValorDisplay());
+        }    
+        this._display.value = this._operacoes.resultado.toString().replace(".", ",");          
+        this._inputNumerico = false;
+        this._ultimaOperacao = "x";
+    }
+
 
     private _ativarBotaoMultiplicacao():void{
         this._btnMultiplicacao.addEventListener("click", ()=>{
-            if(this._ultimaOperacao != "x" && this._ultimaOperacao != "="){
-                this._operacoes.somaValor(this._retornaValorDisplay());
-            }else if(this._inputNumerico && this._ultimaOperacao == "x"){
-                this._operacoes.multiplicaValor(this._retornaValorDisplay());
-            }    
-            this._display.value = this._operacoes.resultado.toString().replace(".", ",");          
-            this._inputNumerico = false;
-            this._ultimaOperacao = "x"
+            this._multiplicar();
+        });
+
+        document.addEventListener("keypress", (event)=>{
+            if(event.key == "*"){
+                this._multiplicar();
+            };
         });
     }
+//==================================================================================================================
 
+
+//===============CONFIGURAÇÃO DO BOTÃO DIVIDIR======================================================================
     private _ativarBotaoDivisao():void{
-        
+            
+    }
+//===============CONFIGURAÇÃO DO BOTÃO IGUAL========================================================================
+
+    private _totalizar():void{
+        if(this._inputNumerico && (this._ultimaOperacao == "+" || this._ultimaOperacao == "-")){
+            this._operacoes.somaValor(this._retornaValorDisplay());
+        }else if(this._inputNumerico && this._ultimaOperacao == "x"){
+            this._operacoes.multiplicaValor(this._retornaValorDisplay());
+        }
+
+        this._display.value = this._operacoes.resultado.toString().replace(".", ",");
+        this._ultimaOperacao = "=";
     }
 
     private _ativarBotaoIgual():void{
         this._btnIgual.addEventListener('click', ()=>{
-            if(this._inputNumerico && (this._ultimaOperacao == "+" || this._ultimaOperacao == "-")){
-                this._operacoes.somaValor(this._retornaValorDisplay());
-            }else if(this._inputNumerico && this._ultimaOperacao == "x"){
-                this._operacoes.multiplicaValor(this._retornaValorDisplay());
-            }
+            this._totalizar();
+        });
 
-            this._display.value = this._operacoes.resultado.toString().replace(".", ",");
-            this._ultimaOperacao = "="
+        document.addEventListener("keypress", (event)=>{
+            if(event.key == "Enter"){
+                this._totalizar();
+            }
+        });
+    }
+//===================================================================================================================
+
+//========CONFIGURAÇÃO DO BOTÃO LIMPAR===============================================================================
+
+    private _ativarBotaoLimpar():void{
+        this._btnLimpar.addEventListener("click", ()=>{
+            this._display.value = "0";
+            this._operacoes.memoria = "";
+            this._operacoes.limparResultado();
+            this._inputNumerico = false;
+            this._ultimaOperacao = "";
         });
     }
 
-  
+//===================================================================================================================  
     private _retornaValorDisplay():any{
         return (this._display.value.search(/[,]/) == -1)?parseInt(this._display.value):parseFloat(this._display.value.replace(",","."));
     }
